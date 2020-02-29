816470e66cb44bf986d93992c28c2768266a6753
Danislav Kamenov <danislav.kamenov@gmail.com>
2020-03-01 01:11:53 +0200

    feat: moved seed generation logic into separate class

---
+++ SeedGenerator.js
@@ -0,0 +1,64 @@
+class SeedGenerator {
+    static createSeed(cols, rows, livingCellsPercent) {
+        let seed = SeedGenerator._createEmptySeed(cols, rows);
+
+        for (let i = 0; i < seed.length; i++) {
+            for (let j = 0; j < seed[i].length; j++) {
+                if (Math.random() >= livingCellsPercent) {
+                    seed[i][j] = 1;
+                }            
+            }
+        }
+
+        return seed;
+    }
+
+    static createNextGeneration(seed) {
+        let next = SeedGenerator._createEmptySeed(seed.length, seed[0].length);
+        for (let i = 0; i < seed.length; i++) {
+            for (let j = 0; j < seed[i].length; j++) {
+               let livingNeighbours = SeedGenerator._livingNeighboursCount(seed, i, j);
+               let state = seed[i][j];
+    
+               if (state === 0 && livingNeighbours === 3) {
+                   next[i][j] = 1;
+               } else if (state === 1 && (livingNeighbours < 2 || livingNeighbours > 3)) {
+                   next[i][j] = 0;
+               } else {
+                   next[i][j] = state;
+               }
+            }
+        }
+    
+        return next;
+    }
+
+    static _createEmptySeed(cols, rows) {
+        let seed = new Array(cols);
+        for (let i = 0; i < cols; i++) {
+            seed[i] = new Array(rows);
+            for (let j = 0; j < seed[i].length; j++) {
+                seed[i][j] = 0;
+            }
+        }
+    
+        return seed;
+    }
+
+    static _livingNeighboursCount(seed, x, y) {
+        let livingNeighbours = 0;
+
+        for (let i = -1; i < 2; i++) {
+            for (let j = -1; j < 2; j++) {
+                let cols = (x + i + seed.length) % seed.length;
+                let rows = (y + j + seed[x].length) % seed[x].length;
+    
+               livingNeighbours += seed[cols][rows];       
+            }
+        }
+    
+        //Do not count yourself.
+        livingNeighbours -= seed[x][y];
+        return livingNeighbours;
+    }
+}
